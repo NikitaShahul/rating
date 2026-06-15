8bbe8b120dec4442e8b503a56fc57ab276d36c81
NikitaShahul <nikitosik252024@gmail.com>
2026-06-16 02:58:36 +0300

    Add abstract DataReader base class

---
+++ src/DataReader.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+
+from Types import DataType
+
+
+class DataReader(ABC):
+    @abstractmethod
+    def read(self, path: str) -> DataType:
+        pass
